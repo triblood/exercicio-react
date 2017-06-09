458d2b04f67ff8995f3ac0b798cc59c2d13f9299
Davi Camarinha <davicamarinha@gmail.com>
2017-06-09 19:27:24 -0300

    update

---
+++ ex1/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<h1>Olá react2</h1>, document.getElementById('app'))
---
+++ ex4/component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Primeiro = props => (
+	<div>
+		<h1>Primeiro componente!</h1>
+		<small>teste</small>
+	</div>
+)
+
+const Segundo = props => <h1>Segundo componente</h1>
+
+export { Primeiro, Segundo }
